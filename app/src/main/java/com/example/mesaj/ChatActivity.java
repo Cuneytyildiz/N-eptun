3d6ec7c565367ca01562c738e01662162f1bb9d3
Cüneyt Yıldız <cuneytyildz@gmail.com>
2020-02-09 22:11:19 +0300

    Proje Prototip

---
+++ app/src/main/java/com/example/mesaj/ChatActivity.java
@@ -0,0 +1,143 @@
+package com.example.mesaj;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ChatActivity extends AppCompatActivity {
+
+    String userName,othername;
+    TextView chatUserName;
+    ImageView backImage,sendImage;
+    EditText chatEditText;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference reference;
+    RecyclerView chatRecyView;
+    MesajAdapter mesajAdapter;
+    List<MesajModel> list;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+        tanimla();
+        loadMesaj();
+    }
+
+    public void tanimla(){
+        list = new ArrayList<>();
+        userName = getIntent().getExtras().getString("username");
+        othername = getIntent().getExtras().getString("othername");
+
+        Log.i("alınandegerler : ",userName+"--->"+othername);
+        chatUserName = (TextView)findViewById(R.id.chatUserName);
+        backImage = (ImageView)findViewById(R.id.backImage);
+        sendImage = (ImageView)findViewById(R.id.sendImage);
+        chatEditText = (EditText)findViewById(R.id.chatEditText);
+        chatUserName.setText(othername);
+        backImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChatActivity.this,MainActivity.class);
+                intent.putExtra("kadi",userName);
+                startActivity(intent);
+            }
+        });
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        reference = firebaseDatabase.getReference();
+
+        sendImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String mesaj = chatEditText.getText().toString();
+                chatEditText.setText("");
+                mesajGonder(mesaj);
+            }
+        });
+        chatRecyView = (RecyclerView)findViewById(R.id.chatRecyView);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(ChatActivity.this,1); // Satırda 1 mesaj
+        chatRecyView.setLayoutManager(layoutManager);
+        mesajAdapter = new MesajAdapter(ChatActivity.this,list,ChatActivity.this,userName);
+        chatRecyView.setAdapter(mesajAdapter);
+
+    }
+
+    public void mesajGonder(String text){
+
+        final String key = reference.child("Mesajlar").child(userName).child(othername).push().getKey();
+        final Map messageMap = new HashMap();
+        messageMap.put("text",text);
+        messageMap.put("from",userName);
+
+        reference.child("Mesajlar").child(userName).child(othername).child(key).setValue(messageMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    reference.child("Mesajlar").child(othername).child(userName).child(key).setValue(messageMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                        }
+                    });
+                }
+            }
+        });
+
+
+    }
+
+    public void loadMesaj(){
+        reference.child("Mesajlar").child(userName).child(othername).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                MesajModel mesajModel = dataSnapshot.getValue(MesajModel.class);
+                list.add(mesajModel);
+                mesajAdapter.notifyDataSetChanged();
+                chatRecyView.scrollToPosition(list.size()-1);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mesaj/GirisActivity.java
@@ -0,0 +1,66 @@
+package com.example.mesaj;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class GirisActivity extends AppCompatActivity {
+
+    EditText kullaniciAdiEditText;
+    Button kayitOlButon;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giris);
+        tanimla();
+    }
+
+    public void tanimla(){
+        kullaniciAdiEditText = (EditText)findViewById(R.id.kullaniciAdiEditText);
+        kayitOlButon = (Button)findViewById(R.id.kayitOlButon);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        reference = firebaseDatabase.getReference();
+
+        kayitOlButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = kullaniciAdiEditText.getText().toString();
+                kullaniciAdiEditText.setText("");
+                ekle(username);
+            }
+        });
+    }
+
+    public void ekle(final String kadi){
+        reference.child("Kullanıcılar").child(kadi).child("kullaniciadi").setValue(kadi).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Giriş Başarılı",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(GirisActivity.this,MainActivity.class);
+                    intent.putExtra("kadi",kadi);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Giriş Başarısız",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
+}
